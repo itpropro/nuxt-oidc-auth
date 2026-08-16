227b040538d7ede607f2e4b47c61cba2b118298a
Jan-Henrik Damaschke <jdamaschke@visorian.com>
2026-08-16 14:59:46 +0200

    fix(docs): use system Chrome for OG images

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -106,6 +106,11 @@ export default defineNuxtConfig({
   },
 
   ogImage: {
+    compatibility: {
+      prerender: {
+        browser: 'chrome-launcher',
+      },
+    },
     zeroRuntime: true,
   },
 })
